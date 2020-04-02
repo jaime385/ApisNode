fa221832dd7fc4e54a113289d585f8c3ef5d3109
Jaime385 <47996736+jaime385@users.noreply.github.com>
2020-04-02 11:16:32 -0600

    Database for testing added.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('Public'));
-//app.use(json({limit: '1mb'}));
+app.use(express.json({limit: '1mb'}));
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 const dataUsa = {
     "country": "USA",
@@ -16,20 +19,32 @@ const dataUsa = {
     "casesPerOneMillion": 650,
     "deathsPerOneMillion": 15,
     "firstCase": "\nJan 20 "
-    };
+};
 
-app.get('/search/:country/:all' , (request , response) => {
-    const countrySelected = request.params.country;
-    
-    if (request.params.country == "USA" && request.params.all == "all") {
-        console.log(dataUsa);
+app.get('/search/:country/:element', (request, response) => {
+    const data = request.params;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data); 
+    if (data.country == "USA" && data.element == "all") {
+        console.log(request.params);
+        response.send({
+            dataUsa,
+            "Time" :  Date(data.timestamp)
+        });
+    } else {
+        console.log(request.params);
+        response.send({
+            "status": "failed",
+            "error": `${request.params.element} related to ${request.params.country} does not exists.`,
+            "Time" :  Date(data.timestamp)
+        });
     }
-    response.send(`I have got ${countrySelected}`);
 });
 
-app.get('/api', (request, response) => {
+const dataFrame = {};
+app.get('/api/:word/:num', (request, response) => {
     console.log(request.params);
-    response.send({
-        "saludo": "Hi"
-    });
+    dataFrame[request.params.word] = request.params.num;
+    response.send(dataFrame);
 });
